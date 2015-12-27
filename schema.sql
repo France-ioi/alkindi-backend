161102919a41f76f3df573247b0eef350497a861
Sebastien Carlier <s.carlier@epixode.fr>
2015-12-27 16:57:23 +0000

    work in progress

--- schema.sql
+++ schema.sql
@@ -17,12 +17,17 @@ CREATE TABLE users (
     foreign_id TEXT NOT NULL,
     username TEXT NOT NULL,
     created_at DATETIME NOT NULL,
-    team_id BIGINT NULL,
     is_selected BOOLEAN NOT NULL,
+    team_id BIGINT NULL,
+    draft_source_id BIGINT NULL,
+    draft_backup_id BIGINT NULL,
+    draft_state TEXT NULL,
+    draft_saved_at DATETIME NOT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
 CREATE UNIQUE INDEX ix_users__foreign_id USING btree ON users (foreign_id(64));
 CREATE INDEX ix_users__team_id USING btree ON users (team_id);
+CREATE INDEX ix_users__draft_id USING btree ON users (draft_id);
 
 CREATE TABLE teams (
     id BIGINT NOT NULL AUTO_INCREMENT,
@@ -30,7 +35,7 @@ CREATE TABLE teams (
     created_at DATETIME NOT NULL,
     code TEXT NOT NULL,
     round_id BIGINT NOT NULL,
-    question_id BIGINT,
+    question_id BIGINT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
 CREATE INDEX ix_teams__creator_id USING btree ON teams (creator_id);
@@ -39,6 +44,7 @@ CREATE INDEX ix_teams__round_id USING btree ON teams (round_id);
 CREATE TABLE team_members (
     team_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
+    joined_at DATETIME NOT NULL,
     is_creator BOOLEAN NOT NULL,
     PRIMARY KEY (team_id, user_id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
@@ -50,22 +56,34 @@ CREATE TABLE rounds (
     updated_at DATETIME NOT NULL,
     title TEXT NOT NULL,
     allow_register BOOLEAN NOT NULL,
-    register_from DATE,
-    register_until DATE,
+    register_from DATE NOT NULL,
+    register_until DATE NOT NULL,
     allow_access BOOLEAN NOT NULL,
-    access_from DATE,
-    access_until DATE,
-    min_team_size INTEGER,
-    max_team_size INTEGER,
-    min_team_ratio DECIMAL(4,3),
+    access_from DATE NOT NULL,
+    access_until DATE NOT NULL,
+    min_team_size INTEGER NOT NULL,
+    max_team_size INTEGER NOT NULL,
+    min_team_ratio DECIMAL(4,3) NOT NULL,
+    questions_path TEXT NOT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
+
+CREATE TABLE badges (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    is_active BOOLEAN NOT NULL,
+    symbol TEXT NOT NULL,
+    round_id BIGINT NOT NULL,
+    PRIMARY KEY (id)
+) CHARACTER SET utf8 ENGINE=InnoDB;
+CREATE INDEX ix_badges__symbol USING btree ON badges (is_active, symbol(20));
 
 CREATE TABLE questions (
     id BIGINT NOT NULL AUTO_INCREMENT,
     created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
     round_id BIGINT NOT NULL,
     data_path TEXT NOT NULL,
+    data TEXT NOT NULL,
     is_assigned BOOLEAN NOT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
@@ -87,6 +105,7 @@ CREATE TABLE workspace_revisions (
     updated_at DATETIME NOT NULL,
     workspace_id BIGINT NOT NULL,
     owner_id BIGINT NOT NULL,
+    is_draft BOOLEAN NOT NULL,
     is_active BOOLEAN NOT NULL,
     state TEXT NOT NULL,
     PRIMARY KEY (id)
@@ -96,6 +115,10 @@ CREATE INDEX ix_workspace_revisions__owner_id USING btree ON workspace_revisions
 
 ALTER TABLE users ADD CONSTRAINT fk_users__team_id
     FOREIGN KEY ix_users__team_id (team_id) REFERENCES teams(id) ON DELETE SET NULL;
+ALTER TABLE users ADD CONSTRAINT fk_users__draft_source_id
+    FOREIGN KEY ix_users__draft_source_id (draft_source_id) REFERENCES workspace_revisions(id) ON DELETE SET NULL;
+ALTER TABLE users ADD CONSTRAINT fk_users__draft_backup_id
+    FOREIGN KEY ix_users__draft_backup_id (draft_backup_id) REFERENCES workspace_revisions(id) ON DELETE SET NULL;
 ALTER TABLE teams ADD CONSTRAINT fk_teams__creator_id
     FOREIGN KEY (creator_id) REFERENCES users(id);
 ALTER TABLE teams ADD CONSTRAINT fk_teams__round_id
@@ -106,6 +129,8 @@ ALTER TABLE team_members ADD CONSTRAINT fk_team_members__team_id
     FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE;
 ALTER TABLE team_members ADD CONSTRAINT fk_team_members__user_id
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE badges ADD CONSTRAINT fk_badges__round_id
+    FOREIGN KEY (round_id) REFERENCES rounds(id) ON DELETE CASCADE;
 ALTER TABLE questions ADD CONSTRAINT fk_questions__round_id
     FOREIGN KEY (round_id) REFERENCES rounds(id) ON DELETE CASCADE;
 ALTER TABLE workspaces ADD CONSTRAINT fk_workspaces__team_id
