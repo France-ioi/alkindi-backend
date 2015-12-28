3e371a1f4b269abd4d05b24b774eccc2b0af2672
Sebastien Carlier <s.carlier@epixode.fr>
2015-12-28 10:04:24 +0000

    update schema

--- schema.sql
+++ schema.sql
@@ -4,41 +4,40 @@
 # GRANT ALL PRIVILEGES ON alkindi.* TO alkindi@localhost;
 # FLUSH PRIVILEGES;
 
-# DROP TABLE IF EXISTS workspace_revisions CASCADE;
-# DROP TABLE IF EXISTS workspaces CASCADE;
-# DROP TABLE IF EXISTS questions CASCADE;
-# DROP TABLE IF EXISTS rounds CASCADE;
-# DROP TABLE IF EXISTS team_members CASCADE;
-# DROP TABLE IF EXISTS teams CASCADE;
-# DROP TABLE IF EXISTS users CASCADE;
+# echo "select table_name, constraint_name from information_schema.key_column_usage where constraint_name like 'fk_%';" | mysql alkindi | awk '{ print "ALTER TABLE " $1 " DROP FOREIGN KEY " $2 ";" }'
+
+DROP TABLE IF EXISTS badges CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS rounds CASCADE;
+DROP TABLE IF EXISTS team_members CASCADE;
+DROP TABLE IF EXISTS teams CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS workspace_revisions CASCADE;
+DROP TABLE IF EXISTS workspaces CASCADE;
 
 CREATE TABLE users (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    foreign_id TEXT NOT NULL,
-    username TEXT NOT NULL,
     created_at DATETIME NOT NULL,
+    foreign_id TEXT NOT NULL,
     team_id BIGINT NULL,
-    draft_state TEXT NULL,
-    draft_source_id BIGINT NULL,
-    draft_backup_id BIGINT NULL,
-    draft_saved_at DATETIME NOT NULL,
+    username TEXT NOT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
 CREATE UNIQUE INDEX ix_users__foreign_id USING btree ON users (foreign_id(64));
 CREATE INDEX ix_users__team_id USING btree ON users (team_id);
-CREATE INDEX ix_users__draft_id USING btree ON users (draft_id);
 
 CREATE TABLE teams (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    creator_id BIGINT NOT NULL,
     created_at DATETIME NOT NULL,
-    code TEXT NOT NULL,
+    creator_id BIGINT NOT NULL,
     round_id BIGINT NOT NULL,
     question_id BIGINT NULL,
+    code TEXT NOT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
 CREATE INDEX ix_teams__creator_id USING btree ON teams (creator_id);
 CREATE INDEX ix_teams__round_id USING btree ON teams (round_id);
+CREATE INDEX ix_teams__question_id USING btree ON teams (question_id);
 
 CREATE TABLE team_members (
     team_id BIGINT NOT NULL,
@@ -76,6 +75,7 @@ CREATE TABLE badges (
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
 CREATE INDEX ix_badges__symbol USING btree ON badges (is_active, symbol(20));
+CREATE INDEX ix_badges__round_id USING btree ON badges (round_id);
 
 CREATE TABLE questions (
     id BIGINT NOT NULL AUTO_INCREMENT,
@@ -83,8 +83,8 @@ CREATE TABLE questions (
     updated_at DATETIME NOT NULL,
     round_id BIGINT NOT NULL,
     data_path TEXT NOT NULL,
-    data TEXT NOT NULL,
-    is_assigned BOOLEAN NOT NULL,
+    full_data TEXT NOT NULL,
+    team_data TEXT NOT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
 CREATE INDEX ix_questions__round_id USING btree ON questions (round_id);
@@ -101,24 +101,22 @@ CREATE INDEX ix_workspaces__team_id USING btree ON workspaces (team_id);
 
 CREATE TABLE workspace_revisions (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
+    title TEXT NULL,
     workspace_id BIGINT NOT NULL,
-    owner_id BIGINT NOT NULL,
-    is_draft BOOLEAN NOT NULL,
+    created_at DATETIME NOT NULL,
+    creator_id BIGINT NOT NULL,
+    parent_id BIGINT NULL,
     is_active BOOLEAN NOT NULL,
+    is_precious BOOLEAN NOT NULL,
     state TEXT NOT NULL,
     PRIMARY KEY (id)
 ) CHARACTER SET utf8 ENGINE=InnoDB;
 CREATE INDEX ix_workspace_revisions__workspace_id USING btree ON workspace_revisions (workspace_id);
-CREATE INDEX ix_workspace_revisions__owner_id USING btree ON workspace_revisions (owner_id);
+CREATE INDEX ix_workspace_revisions__creator_id USING btree ON workspace_revisions (creator_id);
+CREATE INDEX ix_workspace_revisions__parent_id USING btree ON workspace_revisions (parent_id);
 
 ALTER TABLE users ADD CONSTRAINT fk_users__team_id
     FOREIGN KEY ix_users__team_id (team_id) REFERENCES teams(id) ON DELETE SET NULL;
-ALTER TABLE users ADD CONSTRAINT fk_users__draft_source_id
-    FOREIGN KEY ix_users__draft_source_id (draft_source_id) REFERENCES workspace_revisions(id) ON DELETE SET NULL;
-ALTER TABLE users ADD CONSTRAINT fk_users__draft_backup_id
-    FOREIGN KEY ix_users__draft_backup_id (draft_backup_id) REFERENCES workspace_revisions(id) ON DELETE SET NULL;
 ALTER TABLE teams ADD CONSTRAINT fk_teams__creator_id
     FOREIGN KEY (creator_id) REFERENCES users(id);
 ALTER TABLE teams ADD CONSTRAINT fk_teams__round_id
@@ -137,5 +135,7 @@ ALTER TABLE workspaces ADD CONSTRAINT fk_workspaces__team_id
     FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE;
 ALTER TABLE workspace_revisions ADD CONSTRAINT fk_workspace_revisions__workspace_id
     FOREIGN KEY (workspace_id) REFERENCES workspaces(id) ON DELETE CASCADE;
-ALTER TABLE workspace_revisions ADD CONSTRAINT fk_workspace_revisions__owner_id
-    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE workspace_revisions ADD CONSTRAINT fk_workspace_revisions__creator_id
+    FOREIGN KEY (creator_id) REFERENCES users(id) ON DELETE CASCADE;
+ALTER TABLE workspace_revisions ADD CONSTRAINT fk_workspace_revisions__parent_id
+    FOREIGN KEY (parent_id) REFERENCES workspace_revisions(id) ON DELETE SET NULL;
