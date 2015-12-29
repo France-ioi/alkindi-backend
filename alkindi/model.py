481a01dfae735ef3fcddf8d3d168a59340d99ffd
Sebastien Carlier <s.carlier@epixode.fr>
2015-12-29 18:23:24 +0000

    model: implement join_team

--- alkindi/model.py
+++ alkindi/model.py
@@ -125,21 +125,63 @@ class Model:
         })
         team_id = self.db.insert(query)
         # Create the team_members row.
+        self.add_team_member(team_id, user_id,
+                             is_selected=True, is_creator=True)
+        # Update the user's team_id.
+        self.set_user_team_id(user_id, team_id)
+        return True
+
+    def join_team(self, user_id, team_id, user_badges):
+        """ Add a user to a team.
+            Registration for the team's round must be open.
+            Return a boolean indicating if the team member was added.
+        """
+        # Verify that the user exists and does not already belong to a team.
+        current_team_id = self.get_user_team_id(user_id)
+        if current_team_id is not None:
+            # User is already in a team.
+            return False
+        # Verify that the team exists, and get its round_id.
+        round_id = self.get_team_round_id(team_id)
+        # Verify that the round is open for registration.
+        rounds = self.db.tables.rounds
+        (allow_register,) = self.db.first(
+            self.db.query(rounds)
+                .fields(rounds.allow_register)
+                .where(rounds.id == round_id))
+        if not self.db.view_bool(allow_register):
+            # Registration is closed.
+            return False
+        # Look up the badges that grant access to the team's round, to
+        # figure out whether the user is selected for that round.
+        badges = self.db.tables.badges
+        if len(user_badges) == 0:
+            is_selected = False
+        else:
+            row = self.db.first(self.db.query(badges).fields(badges.id).where(
+                (badges.round_id == round_id) &
+                (badges.symbol.in_(user_badges)) &
+                badges.is_active))
+            is_selected = row is not None
+        # Create the team_members row.
+        self.add_team_member(team_id, user_id, is_selected=is_selected)
+        # Update the user's team_id.
+        self.set_user_team_id(user_id, team_id)
+        return True
+
+    # --- private methods below ---
+
+    def add_team_member(self, team_id, user_id,
+                        is_selected=False, is_creator=False):
         team_members = self.db.tables.team_members
         query = self.db.query(team_members).insert({
             team_members.team_id: team_id,
             team_members.user_id: user_id,
             team_members.joined_at: datetime.utcnow(),
-            team_members.is_selected: True,
-            team_members.is_creator: True
+            team_members.is_selected: is_selected,
+            team_members.is_creator: is_creator
         })
         self.db.execute(query)
-        # Update the user's team_id.
-        users = self.db.tables.users
-        query = self.db.query(users).where(users.id == user_id) \
-            .update({users.team_id: team_id})
-        self.db.execute(query)
-        return True
 
     def get_user_team_id(self, user_id):
         users = self.db.tables.users
@@ -151,6 +193,23 @@ class Model:
             raise InputError('no such user')
         (team_id,) = row
         return team_id
+
+    def set_user_team_id(self, user_id, team_id):
+        users = self.db.tables.users
+        query = self.db.query(users).where(users.id == user_id) \
+            .update({users.team_id: team_id})
+        self.db.execute(query)
+
+    def get_team_round_id(self, team_id):
+        teams = self.db.tables.teams
+        row = self.db.first(
+            self.db.query(teams)
+                .fields(teams.round_id)
+                .where(teams.id == team_id))
+        if row is None:
+            raise InputError('no such team')
+        (round_id,) = row
+        return round_id
 
     def select_round_with_badges(self, badges):
         rounds = self.db.tables.rounds
