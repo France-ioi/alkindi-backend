5d2c4bc73a4c8790d13f6ba0593a0e0b43055bfc
Sebastien Carlier <s.carlier@epixode.fr>
2015-12-30 18:44:56 +0000

    add user contexts and security

--- alkindi/contexts.py
+++ alkindi/contexts.py
@@ -1,5 +1,10 @@
 
-from pyramid.security import unauthenticated_userid
+from pyramid.security import unauthenticated_userid, DENY_ALL, Allow
+
+from alkindi.globals import app
+
+
+ADMIN_GROUP = 'g:admin'
 
 
 def includeme(config):
@@ -10,20 +15,70 @@ class RootContext:
 
     __parent__ = None
     __name__ = None
+    __acl__ = [DENY_ALL]
 
     def __init__(self, request):
         self.request = request
 
     def __getitem__(self, path_element):
         if path_element == 'api':
-            if unauthenticated_userid(self.request) is not None:
-                return ApiContext(self)
+            # API access requires an authenticated user.
+            userid = unauthenticated_userid(self.request)
+            print("api {}".format(userid))
+            if unauthenticated_userid(self.request) is None:
+                raise KeyError
+            return ApiContext(self)
         raise KeyError
 
 
-class ApiContext:
+class ApiContextBase:
+
+    __acl__ = [DENY_ALL]
+
+    def __init__(self, parent, **kwargs):
+        self.__parent__ = parent
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    @property
+    def request(self):
+        return self.__parent__.request
+
+    def allow_user(self, *actions):
+        userid = unauthenticated_userid(self.request)
+        return (Allow, str(userid), actions)
+
+
+class UserApiContext(ApiContextBase):
+
+    @property
+    def __acl__(self):
+        return [
+            (Allow, ADMIN_GROUP, ['read', 'change']),
+            (Allow, self.user_id, ['read', 'change'])
+        ]
+
+
+class UsersApiContext(ApiContextBase):
+
+    def __getitem__(self, path_element):
+        print('users/{}'.format(path_element))
+        return UserApiContext(self, user_id=path_element)
+
+
+class ApiContext(ApiContextBase):
 
     __name__ = 'api'
 
     def __init__(self, parent):
         self.__parent__ = parent
+
+    FACTORIES = {
+        'users': UsersApiContext,
+    }
+
+    def __getitem__(self, path_element):
+        factory = self.FACTORIES.get(path_element)
+        if factory:
+            return factory(self)
+        raise KeyError()
