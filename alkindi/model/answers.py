4273c9ba145e649b6666aa916682ed80770748e7
Sebastien Carlier <s.carlier@epixode.fr>
2016-02-03 13:53:25 +0000

    update the participation when its score is NULL

--- alkindi/model/answers.py
+++ alkindi/model/answers.py
@@ -59,7 +59,8 @@ def grade_answer(db, attempt_id, submitter_id, data, now):
     answer['id'] = db.insert_row(answers, answer)
     # Best score for the participation?
     new_score = Decimal(answer['score'])
-    if not is_training and new_score > participation['score']:
+    if not is_training and (participation['score'] is None or
+                            new_score > participation['score']):
         update_participation(
             db, participation_id, {'score': new_score})
     return answer
